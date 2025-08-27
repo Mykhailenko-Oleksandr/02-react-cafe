11eca2ef67deb6e06e9287cd4e959d86d9be078d
Sapper <saniamykhailenko@gmail.com>
2025-08-27 17:06:42 +0300

    start hw

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,6 @@
-// src/main.tsx
-
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/App";
+import App from "./components/App/App";
 
 // Нормалізація стилів
 import "modern-normalize";
